275865d7f7a8d7c456867fe7ff5e8af7ad28d001
Rob Pickering <rwpickering@gmail.com>
2020-06-02 16:52:19 +0100

    Do not reject unauthorized requests

--- src/requests.js
+++ src/requests.js
@@ -1,11 +1,20 @@
 import axios from 'axios';
+import https from 'https';
+
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
 
 export const loadCollectionRequest = async (collectionName) => {
-  const response = await axios.get(`${process.env.REACT_APP_API_URL}/${collectionName}/`);
+  const response = await axios.get(`${process.env.REACT_APP_API_URL}/${collectionName}/`, {
+    httpsAgent: agent
+  });
   return response.data;
 };
 
 export const loadItemRequest = async (collectionName, itemId) => {
-  const response = await axios.get(`${process.env.REACT_APP_API_URL}/${collectionName}/`);
+  const response = await axios.get(`${process.env.REACT_APP_API_URL}/${collectionName}/`, {
+    httpsAgent: agent
+  });
   return response.data;
 };
